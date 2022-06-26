3ad6cae413743977f5b211db1feca9405652f43a
ivan.kolbukh <ivan.kolbukh@vitechteam.com>
2022-06-26 18:13:43 +0300

    - added flyWay and users profile.

--- main/java/com/example/sweater/service/UserService.java
+++ main/java/com/example/sweater/service/UserService.java
@@ -3,8 +3,13 @@ package com.example.sweater.service;
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repository.UserRepository;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -37,6 +42,11 @@ public class UserService implements UserDetailsService {
 
     userRepository.save(user);
 
+    sendMessage(user);
+    return true;
+  }
+
+  private void sendMessage(User user) {
     if (!StringUtils.isEmpty(user.getEmail())) {
       String message = String.format(
           "Hello, %s! \n" +
@@ -46,7 +56,6 @@ public class UserService implements UserDetailsService {
       );
       mailSender.send(user.getEmail(), "Activation code", message);
     }
-    return true;
   }
 
   public boolean activateUser(String code) {
@@ -61,5 +70,52 @@ public class UserService implements UserDetailsService {
     userRepository.save(user);
 
     return true;
+  }
+
+  public List<User> findAll() {
+    return userRepository.findAll();
+  }
+
+  public void saveUser(User user, String username, Map<String, String> form) {
+    user.setUsername(username);
+
+    Set<String> roles = Arrays.stream(Role.values())
+        .map(Role::name)
+        .collect(Collectors.toSet());
+
+    user.getRoles().clear();
+
+    for (String key : form.keySet()) {
+      if (roles.contains(key)) {
+        user.getRoles().add(Role.valueOf(key));
+      }
+    }
+
+    userRepository.save(user);
+  }
+
+  public void updateProfile(User user, String password, String email) {
+    String userEmail = user.getEmail();
+
+    boolean isEmailChanged =
+        (email != null && !email.equals(userEmail)) || (userEmail != null && userEmail.equals(email));
+
+    if (isEmailChanged) {
+      user.setEmail(email);
+
+      if (!StringUtils.isEmpty(email)) {
+        user.setActivationCode(UUID.randomUUID().toString());
+      }
+    }
+
+    if (!StringUtils.isEmpty(password)) {
+      user.setPassword(password);
+    }
+
+    userRepository.save(user);
+
+    if (isEmailChanged) {
+      sendMessage(user);
+    }
   }
 }
